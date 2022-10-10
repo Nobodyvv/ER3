5b93e1b9afd7a5c810a5a7da25251f7c3d0a30ad
Alex Vuong <113114434+Nobodyvv@users.noreply.github.com>
2022-10-10 14:52:12 +0100

    Music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     public GameObject groundchecker;
     public LayerMask whatIsGround;
 
+    public AudioClip jump;
+    public AudioClip backgroundMusic;
+
+    public AudioSource sfxPlayer;
+    public AudioSource musicPlayer;
+
     float maxSpeed = 5.0f;
     bool isOnGround = false;
 
@@ -24,6 +30,10 @@ public class PlayerController : MonoBehaviour
         playerObject = GetComponent<Rigidbody2D>();
 
         anim = GetComponent<Animator>();
+
+        musicPlayer.clip = backgroundMusic;
+        musicPlayer.loop = true;
+        musicPlayer.Play();
     }
 
     // Update is called once per frame
